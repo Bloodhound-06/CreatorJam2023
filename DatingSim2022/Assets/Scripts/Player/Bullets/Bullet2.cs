309b2eeacbbc3a226574d0a14501a01defa27e42
Bloodhound-06 <ollipolli06@gmail.com>
2023-05-24 09:45:19 +0200

    bullet2 added

---
+++ DatingSim2022/Assets/Scripts/Player/Bullets/Bullet2.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet2 : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public GameObject player;
+    public float damage = 1;
+    public float splashRange = 4;
+    private void Start()
+    {
+        player = GameObject.Find("Players");
+        if (player.transform.rotation.y == 0)
+        {
+            rb.AddForce(speed * new Vector2(1, 1), ForceMode2D.Impulse);
+        }
+        else
+        {
+            rb.AddForce(speed * new Vector2(-1, 1), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (splashRange > 0)
+        {
+            var hitColliders = Physics2D.OverlapCircleAll(transform.position, splashRange);
+            foreach (var hitCollider in hitColliders)
+            {
+                var enemy = hitCollider.GetComponent<Enemy1Health>();
+                if (enemy)
+                {
+                    var closestPoint = hitCollider.ClosestPoint(transform.position);
+                    var distance = Vector3.Distance(closestPoint, transform.position);
+
+                    var damagePercent = Mathf.InverseLerp(splashRange, 0, distance);
+                    enemy.Enemy1Damage(damagePercent * damage);
+                }
+            }
+        }
+        Destroy(gameObject);
+    }
+}
--- DatingSim2022/Assets/Scripts/Player/PlayerShoot.cs
+++ DatingSim2022/Assets/Scripts/Player/PlayerShoot.cs
@@ -4,20 +4,30 @@ public class PlayerShoot : MonoBehaviour
 {
     [Header("Keybinds")]
     public KeyCode shootKey;
+    public KeyCode shootKey2;
 
     [Header("References")]
     public GameObject bullet;
+    public GameObject bullet2;
     public GameObject BulletStoring;
 
     [Header("Vector2")]
     public Vector2 BulletSpawnPosition;
 
+    
     private void Update()
     {
+
         BulletSpawnPosition = new Vector3(transform.position.x, transform.position.y);
         if (Input.GetKeyDown(shootKey))
         {
             Instantiate(bullet, BulletSpawnPosition, transform.rotation, BulletStoring.transform);
         }
+        else if (Input.GetKeyDown(shootKey2))
+        {
+            Instantiate(bullet2, new Vector2(BulletSpawnPosition.x, BulletSpawnPosition.y + 1), transform.rotation, BulletStoring.transform);
+        }
+
+
     }
 }
